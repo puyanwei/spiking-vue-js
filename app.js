db5b9b43be6db4a085d3e53d28bea4b94c78d25c
Puyan Wei <puyanwei@gmail.com>
2018-02-20 09:36:41 +0000

    added job property and a greet method

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
   el: "#app",
   data: {
-    name: "Bob"
+    name: "Bob",
+    job: "The Builder"
+  },
+  methods: {
+    greet: function() {
+      return "Hey man, wassup!";
+    }
   }
 });
