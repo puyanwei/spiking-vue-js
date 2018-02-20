446f296fde7d325bb229da238bf1ef1b7e115719
Puyan Wei <puyanwei@gmail.com>
2018-02-20 11:49:28 +0000

    Computed Properties - put functions in new Vue hash 'computed' and removed brackets from functions in html computed functions will only run the methods that has changes in them, where as ones in the method hashes runs them all

--- app.js
+++ app.js
@@ -5,11 +5,20 @@ new Vue({
     b: 0,
     age: 25
   },
-  methods: {
+  // methods: {
+  //   addToA: function() {
+  //     return this.a + this.age;
+  //   },
+  //   addToB: function() {
+  //     return this.b + this.age;
+  //   }
+  // },
+  computed: {
     addToA: function() {
       return this.a + this.age;
     },
     addToB: function() {
+      console.log(78778);
       return this.b + this.age;
     }
   }
