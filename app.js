74caae4112080eb6cf9188cb5e190fb7c714d288
Puyan Wei <puyanwei@gmail.com>
2018-02-20 11:54:40 +0000

    Dynamic CSS - setup html and vue hashes

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
 new Vue({
   el: "#app",
   data: {
-    a: 0,
-    b: 0,
-    age: 25
+    available: false,
+    nearby: false
   },
-  // methods: {
-  //   addToA: function() {
-  //     return this.a + this.age;
-  //   },
-  //   addToB: function() {
-  //     return this.b + this.age;
-  //   }
-  // },
-  computed: {
-    addToA: function() {
-      return this.a + this.age;
-    },
-    addToB: function() {
-      console.log(78778);
-      return this.b + this.age;
-    }
-  }
+  methods: {},
+  computed: {}
 });
