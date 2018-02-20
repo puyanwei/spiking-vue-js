9b8792f32984f6eeeb00c9a783b343a3418bb7a1
Puyan Wei <puyanwei@gmail.com>
2018-02-20 10:07:44 +0000

    Events - adding buttons to increase or decrease age

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 new Vue({
   el: "#app",
   data: {
-    name: "Bob",
-    job: "The Builder",
-    website: "http://www.google.com",
-    websiteTag: "<a href='http://www.google.com'>GOOGLE</a>"
+    age: 25
   },
-  methods: {
-    greet: function(time) {
-      return "Hey man, wassup! Good " + time + " " + this.name;
-    }
-  }
+  methods: {}
 });
