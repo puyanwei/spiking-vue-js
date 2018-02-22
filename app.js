ae67920a010075bd9cf856bcf795c87b1df8b8e6
Puyan Wei <puyanwei@gmail.com>
2018-02-22 15:19:32 +0000

    Refs - adding the attribute ref into our html tag allows us to refer to it in our app.js with this.  returns all tags with that attribute, and the objects inside them. We can get that value of the text box with this..input.value

--- app.js
+++ app.js
@@ -1,28 +1,10 @@
-Vue.component("greeting", {
-  template:
-    "<p>Hi, I am {{ name }} <button @click='changeName'>Change Name</button></p>",
-  data: function() {
-    return {
-      name: "Chun Li"
-    };
-  },
+new Vue({
+  el: "#app",
+  data: {},
   methods: {
-    changeName: function() {
-      this.name = "E.Honda";
+    readRefs: function() {
+      console.log(this.$refs.input.value);
     }
-  }
-});
-
-var one = new Vue({
-  el: "#app-one",
-  data: {},
-  methods: {},
-  computed: {}
-});
-
-var two = new Vue({
-  el: "#app-two",
-  data: {},
-  methods: {},
+  },
   computed: {}
 });
