327116b66798d3faf562207e1cae57e06b6cc013
Puyan Wei <puyanwei@gmail.com>
2018-02-22 14:42:44 +0000

    Multiple Vue Instances - made two vue instances, made a vue two function which can change the title in vue one

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 var one = new Vue({
   el: "#app-one",
-  data: {},
+  data: {
+    title: "Vue App One"
+  },
   methods: {},
-  computed: {}
+  computed: {
+    greet: function() {
+      return "Hello this is app one";
+    }
+  }
 });
 
 var two = new Vue({
   el: "#app-two",
-  data: {},
-  methods: {},
-  computed: {}
+  data: {
+    title: "Vue App Two"
+  },
+  methods: {
+    changeTitle: function() {
+      one.title = "Title changed from App Two!";
+    }
+  },
+  computed: {
+    greet: function() {
+      return "Hello this is app two";
+    }
+  }
 });
